f4aa903fc88c99adf2cda3fa0badd0c52652af44
Rivindu Ashinsa <ashinsa.rivindu@gmail.com>
2025-10-09 11:21:55 +0530

    commited created date

---
+++ second_year/Week3/Tutorial/Date.java
@@ -0,0 +1,46 @@
+public class Date{
+    private int day; 
+    private int month; 
+    private int year; 
+    public Date(int day, int month, int year){
+        this.day = day; 
+        this.month = month; 
+        this.year = year; 
+    }
+    public int getDay(){
+        return day; 
+    }
+    public int getMonth(){
+        return month;
+    
+    }
+    public int getYear(){
+        return year;       
+    }
+    public void setDay(int day){
+        this.day = day; 
+    }
+    public void setMonth(int month){
+        this.month = month;
+    }
+    public void setyear(int year){
+        this.year = year;
+    }
+
+    public void setDate(int day, int month, int year){
+        this.year = year; 
+        this.month = month; 
+        this.day = day;
+    }
+    public String getDate(){
+        return toString(); 
+    }
+    @Override
+    public String toString(){
+    return "Date{" + 
+    "day=" + day + "\'" + 
+    "month=" + month + "\'" +
+    "year=" + year + "}";
+    } 
+
+}
